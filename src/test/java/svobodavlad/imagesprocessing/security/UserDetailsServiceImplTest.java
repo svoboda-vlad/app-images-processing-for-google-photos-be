14b3c7a372411152e7ddea87010cad4cc1496230
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2021-10-08 12:37:02 +0000

    tests adjusted

--- src/test/java/svobodavlad/imagesprocessing/security/UserDetailsServiceImplTest.java
+++ src/test/java/svobodavlad/imagesprocessing/security/UserDetailsServiceImplTest.java
@@ -7,15 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import svobodavlad.imagesprocessing.security.User.LoginProvider;
+import svobodavlad.imagesprocessing.testutil.SecurityMockUtil;
 import svobodavlad.imagesprocessing.testutil.UnitTestTemplate;
 
 class UserDetailsServiceImplTest extends UnitTestTemplate {
-
-	@Autowired
-	private PasswordEncoder encoder;
 
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -25,21 +21,19 @@ class UserDetailsServiceImplTest extends UnitTestTemplate {
 
 	@Test
 	void testLoadUserByUsernameOk() {
-		User user = new User("user", encoder.encode("password"), LoginProvider.INTERNAL, "User", "User", null, null);
+		User mockedUser = SecurityMockUtil.getMockedDefaultUserInternal();
+		this.given(userRepository.findByUsername(mockedUser.getUsername())).willReturn(Optional.of(mockedUser));
 
-		this.given(userRepository.findByUsername(user.getUsername())).willReturn(Optional.of(user));
-
-		this.assertThat(userDetailsService.loadUserByUsername(user.getUsername())).isEqualTo(user);
+		this.assertThat(userDetailsService.loadUserByUsername(mockedUser.getUsername())).isEqualTo(mockedUser);
 	}
 
 	@Test
 	void testLoadUserByUsernameThrowsException() {
-		User user = new User("user", encoder.encode("password"), LoginProvider.INTERNAL, "User", "User", null, null);
-
-		this.given(userRepository.findByUsername(user.getUsername())).willReturn(Optional.empty());
+		User mockedUser = SecurityMockUtil.getMockedDefaultUserInternal();
+		this.given(userRepository.findByUsername(mockedUser.getUsername())).willReturn(Optional.empty());
 
 		this.assertThatExceptionOfType(UsernameNotFoundException.class).isThrownBy(() -> {
-			userDetailsService.loadUserByUsername(user.getUsername());
+			userDetailsService.loadUserByUsername(mockedUser.getUsername());
 		});
 	}
 
